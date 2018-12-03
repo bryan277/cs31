1159fd01f6baa799aa1611c99a76cfc10729c682
Hans <hfvos90@gmail.com>
2018-12-03 15:13:48 -0800

    last changed

--- gpTables_3.sql
+++ gpTables_3.sql
@@ -28,7 +28,7 @@ CREATE TABLE BADGE (
 CREATE TABLE INSURANCE (
 	InsuranceNo	       	Integer 	    	NOT NULL,
     Vin					Varchar (17)		NOT NULL,
-    InvoiceNo			Integer 			NOT NULL, -- There is no primary key for InvoiceNO?
+    InvoiceNo			Integer 			NULL, 
     CarCondition		Varchar (255) 		NOT NULL, 
     RepairType			Varchar (255) 		NOT NULL,
     CONSTRAINT			INSURANCE_PK		PRIMARY KEY (Vin, InsuranceNo)
@@ -77,9 +77,12 @@ CREATE TABLE MAINTENANCE (
     Mileage				Integer			 	NULL,
     TypeOfService 		Varchar (255)		NOT NULL,
     MaintComment		Varchar (255)		NOT NULL,
-    CONSTRAINT			MAINTENANCE_PK		PRIMARY KEY (Vin, MaintenanceNo),
+    EmployyeeID			Integer 	    	NOT NULL,
+    CONSTRAINT			MAINTENANCE_PK		PRIMARY KEY (Vin, MaintenanceNo, EmployyeeID),
 	CONSTRAINT			MAINTENANCE_VIN_FK	FOREIGN KEY (Vin)
-									REFERENCES CAR (Vin)
+									REFERENCES CAR (Vin),
+	CONSTRAINT			MAINTENANCE_EMP_FK		FOREIGN KEY (EmployyeeID)
+									REFERENCES EMPLOYEE (EmployyeeID)
 );
 
 CREATE TABLE CUSTOMER (
@@ -114,16 +117,16 @@ CREATE TABLE INVOICE (
 
 
 INSERT INTO CAR VALUES (
-	'5XYKWDA74EG536509', 'GT-R', 'Nissan', 'White', '7BZV628', 'Sedan', 12324, '2018-01-14 09:30:00
+	'5XYKWDA74EG536509', 'GT-R', 'Nissan', 'White', '7BZV628', 'Sedan', 12324, '2018-02-15 09:45:00
 ',1);
 
 INSERT INTO CAR VALUES (
-	'5FNYF3H23EB079746', 'ACURA', 'Tsx', 'Silver', '7BZV690', 'Sedan', 12433, '2018-01-14 09:35:00
+	'5FNYF3H23EB079746', 'ACURA', 'Tsx', 'Silver', '7BZV690', 'Sedan', 12433, '2018-02-17 09:30:00
 ',1);
 
 INSERT INTO CAR VALUES (
 	'5NPE34AF2FH023783', 'BMW', 'M3', 'Black', '7CSN910', 'Sedan', 69342, '2018-01-14 09:35:00
-',2);
+',1);
 
 INSERT INTO CAR VALUES (
 	'3GKEC16Z96G247342', 'Toyota', 'Tundra', 'White', '7BXP323', 'Truck', 23456, '2018-01-14 09:35:00
@@ -142,7 +145,7 @@ INSERT INTO EMPLOYEE VALUES (
 	45, 'Bradley Vega', 'Agent', 'No', '2014-02-15 09:30:00','257423423', 35000.00, null);
 
 INSERT INTO EMPLOYEE VALUES (
-	23, 'Martin E. Mayes', 'Manager', 'Yes', '2010-03-1 09:30:00','517481234', 60000.00, null);
+	23, 'Martin E. Mayes', 'Manager', 'Yes', '2010-03-01 09:30:00','517481234', 60000.00, null);
 
  
 INSERT INTO BADGE VALUES (
@@ -173,81 +176,62 @@ INSERT INTO INVENTORY VALUES (
 	5, '1GTEK14X75Z242025', '2018-01-14 09:30:00', '2018-01-15 09:28:00', 1
 );
 
-CREATE TABLE INSURANCE (
-	InsuranceNo	       	Integer 	    	NOT NULL,
-    Vin					Varchar (17)		NOT NULL,
-    InvoiceNo			Integer 			NOT NULL, -- There is no primary key for InvoiceNO?
-    CarCondition		Varchar (255) 		NOT NULL, 
-    RepairType			Varchar (255) 		NOT NULL,
-    CONSTRAINT			INSURANCE_PK		PRIMARY KEY (Vin, InsuranceNo)
-);
-
 
 INSERT INTO INSURANCE VALUES (
-	1876574569, '5XYKWDA74EG536509', 2, 'Flawless', 'No repairment required' 
+	1876574569, '5XYKWDA74EG536509', 1, 'Flawless', 'No repairment required' 
 );
 INSERT INTO INSURANCE VALUES (
 	1435645754, '5FNYF3H23EB079746', 2, 'Flawless', 'No repairment required' 
 );
 INSERT INTO INSURANCE VALUES (
-	1875678452, '5NPE34AF2FH023783', 2, 'Flawless', 'No repairment required' 
+	1875678452, '5NPE34AF2FH023783', null, 'Flawless', 'No repairment required' 
 );
 INSERT INTO INSURANCE VALUES (
-	1786758653, '3GKEC16Z96G247342', 2, 'Flawless', 'No repairment required' 
+	1786758653, '3GKEC16Z96G247342', null, 'Flawless', 'No repairment required' 
 );
 INSERT INTO INSURANCE VALUES (
-	1987644532, '1GTEK14X75Z242025', 2, 'Flawless', 'No repairment required' 
+	1987644532, '1GTEK14X75Z242025', null, 'Flawless', 'No repairment required' 
 );
 
 
 INSERT INTO MAINTENANCE VALUES (
-	1, '2018-01-01 09:30:00', '5XYKWDA74EG536509', 1001, 'No service required', 'No comment'
+	1, '2018-01-01 09:30:00', '5XYKWDA74EG536509', 1001, 'No service required', 'No comment', 46 
 );
 INSERT INTO MAINTENANCE VALUES (
-	2, '2018-01-02 09:30:00', '5FNYF3H23EB079746', 14092, 'Oil change', 'No comment'
+	2, '2018-01-02 09:30:00', '5FNYF3H23EB079746', 14092, 'Oil change', 'No comment', 46
 );
 INSERT INTO MAINTENANCE VALUES (
-	3, '2018-01-03 09:30:00', '5NPE34AF2FH023783', 11001, 'Oil change', 'No comment'
+	3, '2018-01-03 09:30:00', '5NPE34AF2FH023783', 11001, 'Oil change', 'No comment', 46
 );
 INSERT INTO MAINTENANCE VALUES (
-	4, '2018-01-04 09:30:00', '3GKEC16Z96G247342', 1871, 'No service required', 'No comment'
+	4, '2018-01-04 09:30:00', '3GKEC16Z96G247342', 1871, 'No service required', 'No comment' ,45
 );
 INSERT INTO MAINTENANCE VALUES (
-	5, '2018-01-05 09:30:00', '1GTEK14X75Z242025', 30005, 'Replaced brakes, oil change', 'New brake pads that should last for the next 40,000 miles'
+	5, '2018-01-05 09:30:00', '1GTEK14X75Z242025', 30005, 'Replaced brakes, oil change', 'New brake pads that should last for the next 40,000 miles' , 45
 );
+
+
+
+-- INSERT INTO RENTALPRICE VALUES (
+-- 	
+
+-- );
 
 INSERT INTO CUSTOMER VALUES (
 	3543, 'Jason Bourne', '212 Castle St, Anaheim CA 92833', 'X9570908', '202-555-0153');
 
 INSERT INTO CUSTOMER VALUES (
 	2343, 'Frances Hawks', '4433 Goldie Lane, Anaheim CA 92823', 'O2590506', '513-463-7896');
- 
- 
- 
--- CREATE TABLE INVOICE (
--- 	InvoiceNo			Integer				NOT NULL,
---     Price				Float				NULL,        -- int to float
---     DateCheckout		Datetime			NOT NULL,
---     Gas					Decimal				NULL,
---     CustomerID			Integer				NOT NULL,
---     InsuranceNo			Integer				NOT NULL,
---     Vin					Varchar (17)		NOT NULL,
---     EmployeeID			Integer				NOT NULL,
---     CONSTRAINT			INVOICE_PK			PRIMARY KEY (InvoiceNo, CustomerID, InsuranceNo, Vin, EmployeeID),
---     CONSTRAINT			INVOICE_CUS_FK		FOREIGN KEY (CustomerID)
--- 									REFERENCES CUSTOMER (CustomerID),
---     CONSTRAINT			INVOICE_INS_FK		FOREIGN KEY (Vin, InsuranceNo)
--- 									REFERENCES INSURANCE (Vin, InsuranceNo),
--- 	CONSTRAINT			INVOICE_VIN_FK		FOREIGN KEY (Vin)
--- 									REFERENCES CAR (Vin),
--- 	CONSTRAINT			INVOICE_EMP_FK		FOREIGN KEY (EmployeeID)
--- 									REFERENCES EMPLOYEE (EmployyeeID)
--- );
 
 
 INSERT INTO INVOICE VALUES (
-	1, 29.99, '2018-01-14 09:45:00', 35, 1, 1, '5XYKWDA74EG536509', 123
+	1, 34.99, '2018-02-15 09:45:00', 35, 3543, 1876574569, '5XYKWDA74EG536509', 46
 );
+INSERT INTO INVOICE VALUES (
+	2, 29.99, '2018-02-17 09:30:00', 32, 2343, 1435645754, '5FNYF3H23EB079746', 45
+);
+
+
 -- For error 1452. Create new sql file and type in SET FOREIGN_KEY_CHECKS=0;
 
 
